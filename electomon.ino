c345ffcdaab348405992f277da1f0feb42027c77
Miedwar Meshbesher <quatrix@Miedwars-iMac.local>
2015-12-24 18:03:54 +0200

    Sending data to emoncms

--- electomon.ino
+++ electomon.ino
@@ -5,15 +5,43 @@
 #include <LiquidCrystal_I2C.h>
 #include <SoftwareSerial.h>
 
+#define EMONCMS_KEY "3873f22d0d8051a59794892815a6f2bf"
+
 SoftwareSerial esp8266_serial(2,3);
 
 LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);  // Set the LCD I2C address
 EnergyMonitor emon1;             // Create an instance
 
-#define WIFI_SSID "evilnet"
-#define WIFI_PASS "evil@me4ever"
+String genStatsRequest(float realPower, float apparentPower, float powerFactor, float Vrms, float Irms) {
+    String cmd = String("GET /input/post.json?json={");
 
-void get(String dst_ip) {
+    cmd += "realPower:";
+    cmd += realPower;
+    
+    cmd += ",apparentPower:";
+    cmd += apparentPower;
+
+    cmd += ",powerFactor:";
+    cmd += powerFactor;
+
+    cmd += ",Vrms:";
+    cmd += Vrms;
+
+    cmd += ",Irms:";
+    cmd += Irms;
+
+    cmd += "}&apikey=";
+    cmd += EMONCMS_KEY;
+    cmd += " HTTP/1.0\r\n";
+    cmd += "Host: emoncms.org\r\n";
+    cmd += "User-Agent: Arduino-ethernet\r\n";
+    cmd += "Connection: close\r\n";
+    cmd += "\r\n";
+
+    return cmd;
+}
+
+boolean send(String dst_ip, String request) {
     String cmd = "AT+CIPSTART=\"TCP\",\"";
     cmd += dst_ip;
     cmd += "\",80";
@@ -22,97 +50,32 @@ void get(String dst_ip) {
     Serial.println(cmd);
 
     if(esp8266_serial.find("Error"))
-         return;
+         return false;
 
-    cmd = "GET / HTTP/1.0\r\n\r\n";
     esp8266_serial.print("AT+CIPSEND=");
-    esp8266_serial.println(cmd.length());
+    esp8266_serial.println(request.length());
 
     if(esp8266_serial.find(">")) {
         Serial.print(">");
     } else {
         esp8266_serial.println("AT+CIPCLOSE");
         Serial.println("connect timeout");
-        delay(1000);
-        return;
-    }
-
-    esp8266_serial.print(cmd);
-    delay(2000);
-    while (esp8266_serial.available()) {
-        char c = esp8266_serial.read();
-        Serial.write(c);
-        delay(10);
-        if(c=='\r') Serial.print('\n');
-    }
-
-    Serial.println("====");
-    delay(1000);
-}
-
-boolean connectWIFI() {
-    esp8266_serial.println("AT+CWMODE=1");
-
-    String cmd="AT+CWJAP=\"";
-    cmd+=WIFI_SSID;
-    cmd+="\",\"";
-    cmd+=WIFI_PASS;
-    cmd+="\"";
-
-    esp8266_serial.println(cmd);
-    Serial.println(cmd);
-    delay(5000);
-
-    if (!esp8266_serial.find("OK"))
         return false;
+    }
 
-    return esp8266_serial.find("GOT IP");
+    Serial.println(request);
+    esp8266_serial.print(request);
 }
 
-void setup()
-{  
+void setup() {  
     lcd.begin(20,4);   // initialize the lcd for 16 chars 2 lines, turn on backlight
 
     Serial.begin(9600);
     esp8266_serial.begin(115200);
-    esp8266_serial.setTimeout(1000);
+    //esp8266_serial.setTimeout(1000);
 
-/*
-    while(!Serial);
-    while(!esp8266_serial);
-
-    while(esp8266_serial.available()>0)
-        esp8266_serial.read();
-
-    delay(1000);
-
-    esp8266_serial.println("AT+RST");
-    Serial.println("resetting esp");
-    esp8266_serial.flush();
-
-    if (esp8266_serial.find("ready")) {
-        Serial.println("esp ready");
-    } else {
-        Serial.println("esp not ready");
-        while(1);
-    }
-
-    delay(1000);
-
-    if (!connectWIFI()) {
-        Serial.println("can't connect wifi");
-        while(1);
-    }
-
-    Serial.println("connected to wifi");
-
-*/
-
-
-    //emon1.voltage(2, 94, 0.5);  // Voltage: input pin, calibration, phase_shift
     emon1.voltage(2, 219, 1.7);  // Voltage: input pin, calibration, phase_shift
     emon1.current(1, 16.15);       // Current: input pin, calibration.
-
 }
 
 
@@ -122,10 +85,7 @@ void writeToLCD(String msg, int line) {
 }
 
 
-void loop() {
-    get("207.210.101.146");
-    delay(1000);
-/*
+void serialSpy() {
     if(esp8266_serial.available()) {  
         while(esp8266_serial.available()) {  
             char c = esp8266_serial.read(); // read the next character.  
@@ -142,14 +102,17 @@ void loop() {
         while(Serial.available()) {  
             command+=(char)Serial.read();  
         }  
+        Serial.println("sending: " + command);
 
         esp8266_serial.println(command); // send the read character to the esp8266_serial  
     }  
+}
 
-*/
-/*
+int counts = 0;
+
+void loop() {
+    //serialSpy();
     emon1.calcVI(100,2000);         // Calculate all. No.of half wavelengths (crossings), time-out
-    //emon1.serialprint();           // Print out all variables (realpower, apparent power, Vrms, Irms, power factor)
 
     float realPower       = emon1.realPower;        //extract Real Power into variable
     float apparentPower   = emon1.apparentPower;    //extract Apparent Power into variable
@@ -163,5 +126,6 @@ void loop() {
     writeToLCD("V(rms):   " + String(Vrms), 2);
     writeToLCD("I(rms):   " + String(Irms), 3);
 
-*/
+    send("80.243.190.58",genStatsRequest(realPower, apparentPower, powerFactor, Vrms, Irms));
 }
+
